c4fcebeeb3b822cc251a3db7938617ba52e3b947
Bekzod Abdulkhaev <bekzodabdulkhaev@Bekzods-MacBook-Pro.local>
2023-07-23 21:47:49 +0900

    solved

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,104 +1,134 @@
-import {useEffect, useRef, useState} from 'react';
-// import { Button, Main, Title } from './TimerStyle';
-import { FaTrashAlt } from 'react-icons/fa';
-import { Button, Main, Title } from './TimerStyle';
+import { useEffect, useRef, useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+import { Button, Main, Title } from "./TimerStyle";
 
 function timeListInfo() {
-    const timeListText = [];
-    for (let i = 0; i < 0 ; i++) {
-        timeListText.push({
-            id: i,
-            text: "Result:" + (i + 1),
-        });
-    }
+  const timeListText = [];
+  for (let i = 0; i < timeListText.length; i++) {
+    timeListText.push({
+      id: i,
+      text: "Timer: " + (i + 1),
+    });
+  }
 
-    return timeListText;
+  return timeListText;
 }
 
-export default function TimerComp(){
-    const [startTime, setStartTime] = useState(null);
-    const [liveTime, setLiveTime] = useState(null);
-    const [todoList, setTodoList] = useState(timeListInfo);
-    const [text, setText] = useState("");  
-    const deleteTodo = (id) => {
-        setTodoList(todoList.filter((item) => item.id !== id));
-        };    
+export default function TimerComp() {
+  const [startTime, setStartTime] = useState(null);
+  const [liveTime, setLiveTime] = useState(null);
+  const [todoList, setTodoList] = useState(timeListInfo());
+  const [text, setText] = useState("");
 
-    useEffect(() => {
-        handleStart();
-        return () => clearInterval(secondSetting.current);
-    }, []);
-    
-    const handleStart = () => {    
-        clearInterval(secondSetting.current);
-        interval = setInterval(startTime, 10)
+  const deleteTodo = (id) => {
+    setTodoList(todoList.filter((item) => item.id !== id));
+  };
 
-        secondSetting.current = setInterval(() =>{
-            
-            setLiveTime((prev)=> prev + 1);
-            console.log(startTime);
-        }, 10)
+  useEffect(() => {
+    handleStart();
+    return () => clearInterval(secondSetting.current);
+  }, []);
 
-        
-        
-    };
+  const handleStart = () => {
+    clearInterval(secondSetting.current);
+    const startTime = Date.now();
+    setStartTime(startTime);
 
-    const secondSetting = useRef();
+    secondSetting.current = setInterval(() => {
+      setLiveTime(Date.now());
+    }, 10);
+  };
 
-    const handlePause = () => {
-        clearInterval(secondSetting.current);       
-    }
+  const secondSetting = useRef();
 
-    const handleReset = () => {
-        clearInterval(secondSetting.current);
-        setStartTime(Date.now);        
-        secondSetting.current = setInterval(() => {
-            setLiveTime(Date.now());
-        }, 10);
-    }
+  const handlePause = () => {
+    clearInterval(secondSetting.current);
+  };
 
-    let timeShow = 0,
-        interval
+  const handleReset = () => {
+    clearInterval(secondSetting.current);
+    setStartTime(Date.now());
+    secondSetting.current = setInterval(() => {
+      setLiveTime(Date.now());
+    }, 10);
+  };
 
+  let timeShow = 0;
 
+  if (startTime != null && liveTime != null) {
+    timeShow = (liveTime - startTime) / 1000;
+  }
 
-
-        if (startTime != null && liveTime != null) {
-            timeShow = (liveTime - startTime) / 1000;
-        }
-
-    
-    return(
-        <Main>
-            <div style={{display:'flex',flexDirection:'column', justifyContent:'left'}}>
-            <Title><h1>Timer: <div style={{marginLeft:'30px',color:'#00FFFF' }}>{timeShow}</div></h1></Title>
-            <div className='Buttonwrap'>
-            <Button onClick={handleStart}>Start</Button>
-            <Button onClick={handlePause}>Pause</Button>
-            <Button onClick={handleReset}>Reset</Button>
-            <Button 
-                onClick={() => {
-                    setText("");
-                    setTodoList([
-                        {
-                            id: todoList.length,
-                            text: text,
-                        }, 
-                        ...todoList,
-                    ])
+  return (
+    <Main>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "left",
+        }}
+      >
+        <Title>
+          <h1>
+            Timer:{" "}
+            <div style={{ marginLeft: "30px", color: "#00FFFF" }}>
+              {timeShow}
+            </div>
+          </h1>
+        </Title>
+        <div className="Buttonwrap">
+          <Button onClick={handleStart}>Start</Button>
+          <Button onClick={handlePause}>Pause</Button>
+          <Button onClick={handleReset}>Reset</Button>
+          <Button
+            onClick={() => {
+              setText("");
+              setTodoList([
+                {
+                  id: todoList.length,
+                  text: "Result: " + timeShow.toFixed(2),
+                },
+                ...todoList,
+              ]);
+            }}
+          >
+            Add
+          </Button>
+        </div>
+        <div className="resultWrapper" style={{}}>
+          <h2>Results:</h2>
+          {todoList.map((item) => (
+            <div
+              style={{
+                padding: "7px 0",
+                display: "flex",
+                justifyContent: "space-between",
+                color: "#778899",
+                alignItems: "center",
+              }}
+              key={item.id}
+            >
+              {item.text}
+              <button
+                style={{
+                  border: "none",
+                  backgroundColor: "transparent",
+                  paddingRight: "10px",
                 }}
-                >Add</Button>
-                </div>
-            <div className='resultWrapper' style={{}}><h2>Results:</h2>{todoList.map((item) => (
-                <div style={{padding:'7px 0' ,display:'flex', justifyContent:'space-between',color:'#778899', alignItems:'center'}} key={item.id}>{liveTime}<button style={{border:'none', backgroundColor:'transparent', paddingRight:'10px' }}>
-                <FaTrashAlt style={{color:'darkgrey', fontSize:'17px',cursor: 'pointer'}} 
-                    onClick={() => deleteTodo(item.id)} />
-            </button></div>
-            ))}
-                
-            </div></div>
-
-
-        </Main>
-    )
+              >
+                <FaTrashAlt
+                  style={{
+                    color: "darkgrey",
+                    fontSize: "17px",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => deleteTodo(item.id)}
+                />
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Main>
+  );
 }
